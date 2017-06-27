12feaf4266c04ca1703ac65add7980a1245b22f7
jimi-f <jimifolarin@gmail.com>
2017-06-27 15:32:17 -0700

    updates

--- start/start.py
+++ start/start.py
@@ -34,24 +34,18 @@ def create_account():
             _hashed_password = generate_password_hash(password)
             conn = mysql.connect()
             cursor = conn.cursor()
-            check = "SELECT username FROM users"
-            cursor.execute(check)
-            check_username = cursor.fetchall()
-            for users in check_username:
-                if username not in users:
-                    query = "INSERT INTO users(email,username,password) VALUES(%s,%s,%s)"
-                    args = (email, username, _hashed_password)
-                    cursor.execute(query, args)
-                    data = cursor.fetchall()
-                    if data is not None:
-                        conn.commit()
-                        return jsonify({'code': 200, 'message': 'success'})
-                        cursor.close()
-                        conn.close()
-                    else:
-                        return json.dumps({'error': str(data[0])})
-                else:
-                    return jsonify({'username exists'})
+            # query = "INSERT INTO users(email,username,password) VALUES(%s,%s,%s)"
+            # args = (email, username, _hashed_password)
+            # cursor.execute(query, args)
+            cursor.callproc('sp_createUser', (email, username, _hashed_password))
+            data = cursor.fetchall()
+            if len(data) is 0:
+                conn.commit()
+                return jsonify({'code': 200, 'message': 'success'})
+                cursor.close()
+                conn.close()
+            else:
+                return json.dumps({'error': str(data[0])})
         else:
             return jsonify({"Incorrect Method"})
     except Exception as e:
@@ -62,9 +56,37 @@ def create_account():
 def login():
     try:
         if request.method == 'POST':
-            return jsonify({'success: successful login'})
+            entry = request.get_json()
+            username = entry['username']
+            password = entry['password']
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            query = "Select username, password from users where username = %s"
+            args = username
+            cursor.execute(query, args)
+            data = cursor.fetchall()
+            if len(data) > 0:
+                if check_password_hash(str(data[0][1]), password):
+                    return jsonify('login confirmed')
+                    cursor.close()
+                    conn.close()
+                else:
+                    return jsonify('incorrect username or password')
+            else:
+                return jsonify('incorrect username')
         else:
             return jsonify({'error: incorrect method'})
+    except Exception as e:
+        return jsonify({'error', str(e)})
+
+
+@app.route('UploadProfilePhoto', methods='POST')
+def upload_profile_photo():
+    try:
+        if request.method == 'POST':
+            return 'we here'
+        else:
+            return 'you lost'
     except Exception as e:
         return jsonify({'error', str(e)})
 
